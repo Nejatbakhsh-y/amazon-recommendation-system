a0e923de7212dfdaf6bb292c36f5ded8f10298b2
Nejatbakhsh-y <nejatbakhsh.y@gmail.com>
2026-04-02 14:44:31 -0400

    Update 01_build_interactions.sql

--- sql/01_build_interactions.sql
+++ sql/01_build_interactions.sql
@@ -1,1 +1,28 @@
+-- Build interaction scores
 
+WITH weighted_events AS (
+    SELECT
+        user_id,
+        product_id,
+        event_type,
+        CASE
+            WHEN event_type = 'view' THEN 1
+            WHEN event_type = 'click' THEN 3
+            WHEN event_type = 'add_to_cart' THEN 5
+            WHEN event_type = 'purchase' THEN 10
+            ELSE 0
+        END AS event_weight
+    FROM sample_events
+),
+
+aggregated_interactions AS (
+    SELECT
+        user_id,
+        product_id,
+        SUM(event_weight) AS interaction_score
+    FROM weighted_events
+    GROUP BY user_id, product_id
+)
+
+SELECT *
+FROM aggregated_interactions;
